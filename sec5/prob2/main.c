19e535eb1405ffa947b379a4f76a012474a3e6fe
bin-101 <bin101bb@gmail.com>
2024-06-24 22:41:52 +0900

    5-2を解いた

---
+++ sec5/prob2/main.c
@@ -0,0 +1,43 @@
+// teeコマンドを実装する
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/*
+fd=open("test.txt", O_WRONLY | O_APPEND);
+lseek(fd, 0, SEEK_SET);
+- 問題文の通り
+- abcdefghijklmnopqrstuvwxyzxy
+
+fd=open("test.txt", O_WRONLY);
+lseek(fd, 0, SEEK_SET);
+- xycdefghijklmnopqrstuvwxyz
+
+fd=open("test.txt", O_WRONLY | O_APPEND);
+lseek(fd, x, SEEK_SET);
+- x=10,100でも下の出力になった
+- abcdefghijklmnopqrstuvwxyzxy
+*/
+
+int main(int argc, char *argv[])
+{
+    //a-zまでの文字が書き込まれたファイルを作成
+    const int SIZE=26;
+    char buffer[SIZE+1];
+    for (int i=0; i<SIZE; i++){
+        buffer[i]='a'+i;
+    }
+    buffer[SIZE]='\0';
+    int fd=open("test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    write(fd, buffer, SIZE);
+    close(fd);
+
+    fd=open("test.txt", O_WRONLY | O_APPEND);
+    lseek(fd, 0, SEEK_SET);
+    char buffer2[3]={'x','y','\0'};
+    write(fd, buffer2, 2);
+    close(fd);
+
+    return 0;
+}
