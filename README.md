b9209bb5bb4470a1565d26fb27d3365ba49877b7
bin-101 <bin101bb@gmail.com>
2024-11-12 08:11:55 +0900

    24-2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # The Linux Programming Interface Problems
 ## Status
-solved: 26
+solved: 27
 total : 212
 ## List
 - section 1 solved: 0 all: 0
@@ -49,8 +49,8 @@ total : 212
 	- :white_medium_square: 22-1  :white_medium_square: 22-2  :white_medium_square: 22-3  :white_medium_square: 22-4 
 - section 23 solved: 0 all: 4
 	- :white_medium_square: 23-1  :white_medium_square: 23-2  :white_medium_square: 23-3  :white_medium_square: 23-4 
-- section 24 solved: 1 all: 5
-	- :white_check_mark: 24-1  :white_medium_square: 24-2  :white_medium_square: 24-3  :white_medium_square: 24-4  :white_medium_square: 24-5 
+- section 24 solved: 2 all: 5
+	- :white_check_mark: 24-1  :white_check_mark: 24-2  :white_medium_square: 24-3  :white_medium_square: 24-4  :white_medium_square: 24-5 
 - section 25 solved: 0 all: 1
 	- :white_medium_square: 25-1 
 - section 26 solved: 0 all: 4
--- sec24/prob2/main.c
+++ sec24/prob2/main.c
@@ -1,16 +1,24 @@
-// vfork後に、親プロセスと子プロセスがファイルディスクリプタを共有していないことを示せ
-// p552の例24-4を参考にした
-
 #define _GNU_SOURCE
 #include <pwd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <sys/wait.h>
 #include <dirent.h>
 #include <string.h>
+#include <errno.h>
+
+
+/*
+ファイルディスクリプタテーブルはカーネルがプロセス単位に管理するため、vfork後でも親プロセスは子プロセスの影響を受けない
+
+bin101@bin101-Inspiron-16-5635:~/code/LinuxProgrammingInterface/sec24/prob2$ ./a.out
+before vfork
+child process
+child process: Bad file descriptor
+parent process: wait(0) // 出力できている
+*/
 
 int main(int argc, char *argv[])
 {
@@ -20,20 +28,24 @@ int main(int argc, char *argv[])
     pid_t child_pid;
     switch (child_pid=vfork()){
     case -1:
-        perror("vfork");
+        printf("fork: %s",strerror(errno));
         exit(EXIT_FAILURE);
     case 0: //子プロセス
         buffer="child process\n";
         write(STDOUT_FILENO, buffer, strlen(buffer));
         close(STDOUT_FILENO);
-        write(STDOUT_FILENO, buffer, strlen(buffer));
+        if(write(STDOUT_FILENO, buffer, strlen(buffer))==-1){
+            char s[100];
+            snprintf(s, 100, "child process: %s\n", strerror(errno));
+            write(STDERR_FILENO, s, strlen(s));
+        }
         _exit(EXIT_SUCCESS);
     default: //親プロセス
         int status;
         wait(&status);
-        printf("status: %d\n", status);
-        buffer="parent process\n";
-        write(STDOUT_FILENO, buffer, strlen(buffer));
+        char s[100];
+        snprintf(s, 100, "parent process: wait(%d)\n", status);
+        write(STDOUT_FILENO, s, strlen(s));
         exit(EXIT_SUCCESS);
     }
 
